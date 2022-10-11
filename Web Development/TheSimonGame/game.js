c041a241c6cde87c96550b5bc4f4fc0b151b5b6d
Danut Sacu <danut.sacu@gmail.com>
2022-10-11 10:06:08 +0300

    Update Simon game

--- Web Development/TheSimonGame/game.js
+++ Web Development/TheSimonGame/game.js
@@ -6,5 +6,36 @@ function nextSequence() {
     randomNumber = Math.floor(Math.random() * 4);
 }
 
-randomChosenColour = buttonColours[randomNumber];
-gamePattern.push(randomChosenColour);
+// randomChosenColour = buttonColours[randomNumber];
+// gamePattern.push(randomChosenColour);
+
+$(":button");
+
+function playGreen() {
+    userChosenColour = document.querySelector("div.green").getAttribute("ID");;
+    console.log(userChosenColour);
+    $("#green").fadeOut(100).fadeIn(100);
+    var audio = new Audio('sounds/green.mp3');
+    audio.play();
+}
+function playRed() {
+    userChosenColour = document.querySelector("div.red").getAttribute("ID");;
+    console.log(userChosenColour);
+    $("#red").fadeOut(100).fadeIn(100);
+    var audio = new Audio('sounds/red.mp3');
+    audio.play();
+}
+function playYellow() {
+    userChosenColour = document.querySelector("div.yellow").getAttribute("ID");;
+    console.log(userChosenColour);
+    $("#yellow").fadeOut(100).fadeIn(100);
+    var audio = new Audio('sounds/yellow.mp3');
+    audio.play();
+}
+function playBlue() {
+    userChosenColour = document.querySelector("div.blue").getAttribute("ID");;
+    console.log(userChosenColour);
+    $("#blue").fadeOut(100).fadeIn(100);
+    var audio = new Audio('sounds/blue.mp3');
+    audio.play();
+}
