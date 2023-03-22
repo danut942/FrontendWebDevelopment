7765566e697219ea32e3b20fa3ee3253fee04a18
Danut Sacu <danut.sacu@gmail.com>
2023-03-22 17:43:34 +0200

    update mongodb atlass connect url string

--- WebDevelopment/todolist-v2/todolist-v2-starting-files/app.js
+++ WebDevelopment/todolist-v2/todolist-v2-starting-files/app.js
@@ -12,7 +12,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
+// mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
+mongoose.connect("mongodb+srv://danutsacu:Am6tEdSwFd9aWQ8N@cluster0.1dufopp.mongodb.net/todolistDB");
 
 const itemsSchema = {
   name: String
